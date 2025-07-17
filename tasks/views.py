105158cb4db2d363b4f7d8acd1a29f0651d3237a
guille <kondra.guille@gmail.com>
2025-07-17 13:10:59 -0300

    33 minutos

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm #creacion de formulario
-
+from django.contrib.auth.models import User #registro de usuarios
+from django.http import HttpResponse #enviar mensaje por html
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
 def signup(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
-    })
+    
+    if request.method == 'GET': #envia el formulario
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else: #obtiene los datos - POST
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                #register user
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1']) #crea el usuario
+                user.save()
+                return HttpResponse('usuario creado correctamente') 
+            except:
+                return HttpResponse('El usuario ya existe')
+    
+        return HttpResponse('Las contrasenias no concuerdan')
